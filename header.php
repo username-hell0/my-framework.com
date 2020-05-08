ee4e5cd7021f9ce147d6ca159479f5a31ab47b14
Vlad Pomazan <vladpomazan22@gmail.com>
2020-05-08 10:31:36 +0300

    Break the existing layout into components
    Break the layout into components (header, navigation, layout, footer)
    and connect them to the appropriate files to avoid duplication.

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="coffee.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
